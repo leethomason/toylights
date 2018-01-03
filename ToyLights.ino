1f9215dfacb931586a34ca8b87aeeeb12aa484c1
Lee Thomason <leethomason@gmail.com>
2018-01-02 17:54:45 -0800

    make eeprom optional

--- ToyLights.ino
+++ ToyLights.ino
@@ -1,23 +1,39 @@
+#include <Arduino.h>
 #include <FastLED.h>
 #include <Button.h>
 #include "palettes.h"
 #include "effects.h"
 
 //#define FEATURE_SHUTDOWN
+//#define FEATURE_EPROM
+
+#ifdef FEATURE_EPROM
+#include "EEPROM.h"
+#endif
 
 #define LED_PIN     6
 #define NUM_LEDS    60
-#define BUTTON_0_PIN  9 // Color change
+#define BUTTON_0_PIN  9  // Color change
 #define BUTTON_1_PIN  10 // Effect
+#define BUTTON_2_PIN  11 // Brightness
+
+#define ESLOT_INIT      0
+#define ESLOT_PALETTE   1
+#define ESLOT_EFFECT    2
+#define ESLOT_BRIGHT    3
 
 static const uint32_t SHUTDOWN_TIME = uint32_t(26) * uint32_t(60) * uint32_t(1000);
 uint32_t shutdownTime = 0;
 
-#define MASTER_BRIGHTNESS   18   // change this to change overall brightness
+#define MASTER_BRIGHTNESS   24
+#define BRIGHTNESS_1        50
+#define BRIGHTNESS_2        100
+#define BRIGHTNESS_3        200
 
 CRGB leds[NUM_LEDS];
 Button button0(BUTTON_0_PIN, Button::INTERNAL_PULLUP);
 Button button1(BUTTON_1_PIN, Button::INTERNAL_PULLUP);
+Button button2(BUTTON_2_PIN, Button::INTERNAL_PULLUP);
 
 State state[NUM_LEDS];
 SolidEffect solidEffect;
@@ -25,14 +41,105 @@ SinEffect sinEffect;
 IcicleEffect icicleEffect;
 SparkleEffect sparkleEffect;
 
-int currentPalette = 9;
-CRGBPalette16 gPalette = OceanColors_p;
-
-Effect* effect = &sparkleEffect;
-int currentEffect = 3;
-
+CRGBPalette16 gPalette = WhiteColors_p;
+Effect* effect = &solidEffect;
 uint32_t currentTime = 0;
 
+#ifndef FEATURE_EPROM
+// M0 doesn't have EEPROM
+uint8_t eepromEffect = 0;
+uint8_t eepromPalette = 0;
+#endif
+
+int setEffect(int id)
+{
+    id = id % 4;
+    switch(id) {
+        case 0:        effect = &solidEffect;      break;
+        case 1:        effect = &sinEffect;        break;
+        case 2:        effect = &icicleEffect;     break;
+        case 3:        effect = &sparkleEffect;    break;
+    }
+    effect->init(millis(), state, NUM_LEDS);
+#ifdef FEATURE_EPROM
+    EEPROM.update(ESLOT_EFFECT, id);
+#else
+    eepromEffect = id;
+#endif
+    resetBrightness();
+    return id;
+}
+
+
+int setPalette(int id)
+{
+    id = id % 15;
+    switch (id) {
+        case 0:        gPalette = WhiteColors_p;      break;
+        case 1:        gPalette = RedColors_p;        break;
+        case 2:        gPalette = OrangeColors_p;     break;
+        case 3:        gPalette = YellowColors_p;     break;
+        case 4:        gPalette = GreenColors_p;      break;
+        case 5:        gPalette = BlueColors_p;       break;
+        case 6:        gPalette = PurpleColors_p;     break;
+
+        case 7:        gPalette = CloudColors_p;      break;
+        case 8:        gPalette = LavaColors_p;       break;
+        case 9:        gPalette = OceanColors_p;      break;
+        case 10:       gPalette = ForestColors_p;     break;
+        case 11:       gPalette = RainbowColors_p;    break;
+        case 12:       gPalette = PartyColors_p;      break;
+        case 13:       gPalette = HeatColors_p;       break;
+        case 14:       gPalette = CastleColors_p;     break;
+    }
+    effect->init(millis(), state, NUM_LEDS);
+#ifdef FEATURE_EPROM
+    EEPROM.update(ESLOT_PALETTE, id);
+#else
+    eepromPalette = id;
+#endif
+    resetBrightness();
+    return id;
+}
+
+
+uint8_t setBrightness(uint8_t value)
+{
+    FastLED.setBrightness(value);
+#ifdef FEATURE_EPROM
+    EEPROM.update(ESLOT_BRIGHT, value);
+#endif
+    return value;
+}
+
+void resetBrightness()
+{
+    setBrightness(MASTER_BRIGHTNESS);
+}
+
+int getEffect()   {
+#ifdef FEATURE_EPROM
+    return EEPROM.read(ESLOT_EFFECT);
+#else
+    return eepromEffect;
+#endif
+}
+
+int getPalette()  {
+#ifdef FEATURE_EPROM
+    return EEPROM.read(ESLOT_PALETTE);
+#else
+    return eepromPalette;
+#endif
+}
+
+uint8_t getBrightness() {
+#ifdef FEATURE_EPROM
+    return EEPROM.read(ESLOT_BRIGHT);
+#else
+    return FastLED.getBrightness();
+#endif
+}
 
 void setup() {
     FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
@@ -46,50 +153,58 @@ void setup() {
     FastLED.show();
     currentTime = millis();
     shutdownTime = currentTime + SHUTDOWN_TIME;
+    button2.setHoldRepeats(true);
+    button2.setHoldThreshold(1000);
+
+#ifdef FEATURE_EPROM
+    if (EEPROM.read(ESLOT_INIT) != 42) {
+        EEPROM.update(ESLOT_INIT, 42);
+        EEPROM.update(ESLOT_PALETTE, 0);
+        EEPROM.update(ESLOT_EFFECT, 0);
+        EEPROM.update(ESLOT_BRIGHT, 0);
+    }
+    setPalette(EEPROM.read(ESLOT_PALETTE));
+    setEffect(EEPROM.read(ESLOT_EFFECT));
+    setBrightness(EEPROM.read(ESLOT_BRIGHT)); 
+#else
+    setPalette(0);
+    setEffect(0);
+    setBrightness(MASTER_BRIGHTNESS);
+#endif
 }
+
 
 void loop() {
     button0.process();
     if (button0.press()) {
         shutdownTime = currentTime + SHUTDOWN_TIME;
-        currentPalette = (currentPalette+1) % 15;
-
-        switch (currentPalette) {
-          case 0:     gPalette = WhiteColors_p;      break;
-          case 1:     gPalette = RedColors_p;        break;
-          case 2:     gPalette = OrangeColors_p;     break;
-          case 3:     gPalette = YellowColors_p;     break;
-          case 4:     gPalette = GreenColors_p;      break;
-          case 5:     gPalette = BlueColors_p;       break;
-          case 6:     gPalette = PurpleColors_p;     break;
-
-          case 7:     gPalette = CloudColors_p;      break;
-          case 8:     gPalette = LavaColors_p;       break;
-          case 9:     gPalette = OceanColors_p;      break;
-          case 10:    gPalette = ForestColors_p;     break;
-          case 11:    gPalette = RainbowColors_p;    break;
-          case 12:    gPalette = PartyColors_p;      break;
-          case 13:    gPalette = HeatColors_p;       break;
-          case 14:    gPalette = CastleColors_p;     break;
-        }
-
-        effect->init(millis(), state, NUM_LEDS);
+        setPalette(getPalette() + 1);
     }
     button1.process();
     if (button1.press()) {
-        currentEffect = (currentEffect+1) % 4;
-        switch(currentEffect) {
-            case 0:   effect = &solidEffect;        break;
-            case 1:   effect = &sinEffect;          break;
-            case 2:   effect = &icicleEffect;       break;
-            case 3:   effect = &sparkleEffect;      break;
+        setEffect(getEffect() + 1);
+    }
+
+    button2.process();
+    if (button2.press()) {
+        resetBrightness();
+    }
+    if (button2.held()) {
+        switch(button2.nHolds()) {
+        case 1:
+            setBrightness(BRIGHTNESS_1);
+            break;
+        case 2:
+            setBrightness(BRIGHTNESS_2);
+            break;
+        default:
+            setBrightness(BRIGHTNESS_3);
+            break;
         }
-        effect->init(millis(), state, NUM_LEDS);
     }
 
 #ifdef FEATURE_SHUTDOWN
     if (millis() > shutdownTime) {
-        currentPalette = 0;
         for (int i = 0; i < NUM_PALETTE_ENTRIES; i++) {
             gPalette[i] = CRGB::Black;
         }
@@ -107,5 +222,3 @@ void loop() {
         FastLED.show();
     }
 }
-
-
